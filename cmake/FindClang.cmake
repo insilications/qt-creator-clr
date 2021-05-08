3f7aa5bfd92e58303774e770034ab6f0abbc6961
Francisco Boni <boboniboni@gmail.com>
2021-05-08 20:59:56 -0300

    libclang static

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -1,17 +1,17 @@
 find_package(Clang CONFIG)
 
 # silence a lot of warnings from building against llvm
-if(MSVC AND TARGET libclang)
-    target_compile_options(libclang INTERFACE /wd4100 /wd4141 /wd4146 /wd4244 /wd4267 /wd4291)
+if(MSVC AND TARGET libclang_static)
+    target_compile_options(libclang_static INTERFACE /wd4100 /wd4141 /wd4146 /wd4244 /wd4267 /wd4291)
 endif()
 
 option(CLANGTOOLING_LINK_CLANG_DYLIB "Force linking of Clang tooling against clang-cpp" NO)
 
 if (TARGET clangTooling AND NOT CLANGTOOLING_LINK_CLANG_DYLIB)
-  set(CLANG_TOOLING_LIBS libclang clangTooling clangQuery clangIndex)
+  set(CLANG_TOOLING_LIBS libclang_static clangTooling clangQuery clangIndex)
   set(CLANG_FORMAT_LIB clangFormat)
 elseif (TARGET clang-cpp)
-  set(CLANG_TOOLING_LIBS libclang clang-cpp)
+  set(CLANG_TOOLING_LIBS libclang_static clang-cpp)
   set(CLANG_FORMAT_LIB clang-cpp)
 endif()
 
